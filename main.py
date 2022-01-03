e77874670af803892f6e2a945f7ad341e8b98bf1
Bennett-Wendorf <bennettwendorf@gmail.com>
2022-01-03 10:34:16 -0600

    Got the basic label widgets to render properly and removed vscode stuff

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.config import Config
 from kivy.core.window import Window
+from kivy.app import App
 
 from kivy.clock import Clock
 import threading
@@ -8,7 +9,7 @@ import threading
 # Set the default size of the window to 240x160, 1/4 the size of my 3.5" touchscreen module for a Raspberry Pi
 Config.set('graphics', 'width', '240')
 Config.set('graphics', 'height', '160')
-Window.size=(480,320)
+Window.size=(240,160)
 
 class SpotifyWidget(RelativeLayout):
     
@@ -22,9 +23,17 @@ class SpotifyWidget(RelativeLayout):
         Clock.schedule_interval(self.Start_Update_Loop, 10)
 
     def Get_Playing(self):
-        pass
+        print("Running Get_Playing")
 
     def Start_Update_Loop(self, dt):
         update_thread = threading.Thread(target=self.Get_Playing)
         update_thread.setDaemon(True)
         update_thread.start()
+
+class KivySpotifyApp(App):
+    def build(self):
+        super(KivySpotifyApp, self).build()
+        return SpotifyWidget()
+
+if __name__ == '__main__':
+    KivySpotifyApp().run()
