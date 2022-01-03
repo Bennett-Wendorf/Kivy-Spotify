07b2c1cc8cda04f6c4a8893e203a7918427663d7
Bennett-Wendorf <bennettwendorf@gmail.com>
2022-01-03 14:40:20 -0600

    Set up Spotipy for Spotify API integration.

--- main.py
+++ main.py
@@ -1,34 +1,12 @@
-from kivy.uix.relativelayout import RelativeLayout
 from kivy.config import Config
 from kivy.core.window import Window
 from kivy.app import App
-
-from kivy.clock import Clock
-import threading
+from spotify_widget import SpotifyWidget
 
 # Set the default size of the window to 240x160, 1/4 the size of my 3.5" touchscreen module for a Raspberry Pi
 Config.set('graphics', 'width', '240')
 Config.set('graphics', 'height', '160')
 Window.size=(240,160)
-
-class SpotifyWidget(RelativeLayout):
-    
-    def __init__(self, **kwargs):
-        # Auth stuff
-
-        self.Get_Playing()
-
-        super(SpotifyWidget, self).__init__(**kwargs)
-
-        Clock.schedule_interval(self.Start_Update_Loop, 10)
-
-    def Get_Playing(self):
-        print("Running Get_Playing")
-
-    def Start_Update_Loop(self, dt):
-        update_thread = threading.Thread(target=self.Get_Playing)
-        update_thread.setDaemon(True)
-        update_thread.start()
 
 class KivySpotifyApp(App):
     def build(self):
---
+++ spotify_widget.py
@@ -0,0 +1,38 @@
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.clock import Clock
+import threading
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import yaml
+
+class SpotifyWidget(RelativeLayout):
+
+    spotify = None
+    
+    def __init__(self, **kwargs):
+        # Auth stuff
+        self.spotify = self.Spotify_Auth()
+
+        print(self.Get_Playing()['item']['name'])
+
+        super(SpotifyWidget, self).__init__(**kwargs)
+
+        Clock.schedule_interval(self.Start_Update_Loop, 10)
+
+    def Get_Playing(self):
+        print("Running Get_Playing")
+        return self.spotify.current_playback()
+
+    def Start_Update_Loop(self, dt):
+        update_thread = threading.Thread(target=self.Get_Playing)
+        update_thread.setDaemon(True)
+        update_thread.start()
+
+    def Spotify_Auth(self):
+        stream = open('./settings.yaml', 'r')
+        settings = yaml.load(stream, Loader=yaml.CLoader)
+
+        return spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=settings['client_id'],
+                                                          client_secret=settings['client_secret'],
+                                                          redirect_uri='http://localhost:8888/redirect',
+                                                          scope='user-library-read streaming app-remote-control user-read-playback-state'))
